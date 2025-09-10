86c3098d7f910e20d60c81b44825abce5bdcdfc1
luxintao <15920824726@163.com>
2025-09-10 21:23:08 +0800

    master新增

--- static/changeText.js
+++ static/changeText.js
@@ -3,12 +3,14 @@
  */
 define(function (require, exports, module) {
     var init= function() {
+        var master = ['master新增']
         var textContent = [
             'yes it works',
             'seajs demo',
             'it is a lucky day',
             'wish this help you',
             'thank you for reading'
+            'master新增内容'
         ];
         var index = Math.floor(Math.random()*textContent.length);
         return textContent[index];
